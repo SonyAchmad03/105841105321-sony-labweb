b4a99a1739ce9a2948d08bcd02d781cbc70417e3
SonyAchmad03 <105841105321@student.unismuh.ac.id>
2024-06-14 12:08:59 +0700

    box view

--- App.js
+++ App.js
@@ -1,28 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-export default function App() {
+const App = () => {
   return (
     <View
       style={{
-        flex: 1,
-        flexDirection: "row",
-      }}
-    >
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "red",
-        }}
-      ></View>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "blue",
-        }}
-      ></View>
+      flex:1
+    }}>
+      <View style={{
+        flex:2,
+      }}>
+      <View style={{
+        width:100,height:100,backgroundColor:'red' 
+      }}>
+      </View>
+      </View>
+      <View style={{
+        flex:3,
+        justifyContent:'flex-end',
+        alignItems:'flex-end'
+      }}>
+      <View style={{
+        width:100,height:100,backgroundColor:'blue'
+        
+      }}>
+      </View>
+      </View>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({});
+export default App
+
+const styles = StyleSheet.create({})
